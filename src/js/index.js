04d01cc8618e37b02e43e5596d0665e0089447a5
Daniel <kuruchbekov2006dan@gmail.com>
2022-05-09 14:46:49 +0600

    fixing bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
-import fetchWeather from './functions/WeatherFetch';
+import fillHtml from './components/Card';
 
+const WEATHER_API = '1c3d95576952b1e721446421b520e447';
+const fetchWeather = async (searchValue = 'Naryn') => {
+	try {
+		const res = await fetch(`
+		https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${WEATHER_API}
+		`);
+		const data = await res.json();
+		console.log(data);
+		fillHtml(data);
+	} catch (err) {
+		console.error(err);
+	}
+};
 fetchWeather();
-
 const searchBtn = document.getElementById('searchBtn');
 const searchInput = document.getElementById('searchInput');
 const search = (e) => {
@@ -12,4 +24,3 @@ const search = (e) => {
 };
 
 searchBtn.addEventListener('click', search);
-export default search;
