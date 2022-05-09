e2e6e91900460ac813e504f2c7c6e99f2824f8da
Daniel <kuruchbekov2006dan@gmail.com>
2022-05-09 14:41:35 +0600

    fixing bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
-import searchBtn from './functions/Search';
 import fetchWeather from './functions/WeatherFetch';
 
 fetchWeather();
+
+const searchBtn = document.getElementById('searchBtn');
+const searchInput = document.getElementById('searchInput');
+const search = (e) => {
+	e.preventDefault();
+	const value = searchInput.value;
+	searchInput.value = '';
+	fetchWeather(value);
+};
+
+searchBtn.addEventListener('click', search);
+export default search;
